53bfb7b606ed83e8520f0b8dbb01e89b953ee89e
Courtney Adams <cadams8238@gmail.com>
2018-05-09 11:58:38 -0700

    added showProperty function

--- gistObjectDrills.js
+++ gistObjectDrills.js
@@ -1,12 +1,59 @@
 'use strict';
 
-let loaf = {
-  flour: 300,
-  water: 210,
-  hydration: function() {
-    return this.water / this.flour * 100;
-  }
+
+//QUESTION 1 ---------------------------------------------
+
+// let loaf = {
+//   flour: 300,
+//   water: 210,
+//   hydration: function() {
+//     return this.water / this.flour * 100;
+//   }
+// }
+
+// console.log(`${loaf.flour}, ${loaf.water}`);
+// console.log(loaf.hydration);
+
+
+
+//QUESTION 2 ---------------------------------------------
+
+const obj = {
+	foo: 1,
+	bar: 2,
+	fum: 3,
+	quux: 'none',
+	spam: true
 }
 
-console.log(`${loaf.flour}, ${loaf.water}`);
-console.log(loaf.hydration);
+function showProperty(obj) {
+
+	for (let key in obj) {
+		console.log(`${key}: ${obj[key]}`);
+	}
+
+}
+
+showProperty(obj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
